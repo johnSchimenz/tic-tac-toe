c644199dd808dbbfb78e6b6f1544239c66faf27e
John Schimenz <john.schimenz@gmail.com>
2023-09-01 07:00:50 -0500

    Create some structure for objects gameBoard Player and gameFlow

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 "use strict";
 
+// Gameboard object
+const gameBoard = (() => {
+    const cellArray = ["", "", "", "", "", "", "", "", ""];
+    return cellArray;
+})();
+
+// Players objects
+const Player = () => {
+    
+}
+
+// Game-flow object
+const gameFlow = (() => {
+    const playerOne
+})();
