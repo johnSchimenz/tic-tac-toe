228058c5c555012eb62448bd9e2124af5a024546
John Schimenz <john.schimenz@gmail.com>
2023-09-03 12:39:46 -0500

    Pseudotext baseline for gameFlow

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 
 // Gameboard object
 const gameBoard = (() => {
-    const cellArray = ["", "", "", "", "", "", "", "", ""];
-    return cellArray;
+    const arrayGameBoard = ["", "", "", "", "", "", "", "", ""];
+    return arrayGameBoard;
 })();
 
 // Players objects
@@ -13,5 +13,12 @@ const Player = (name, marker) => {
 
 // Game-flow object
 const gameFlow = (() => {
+    // 1. Player X selects an cell
     
+    // 2. Check whether you have 3-in-a-row
+        // 2a. If yes:
+            // print "Player X wins"
+            // prompt whether to play again
+        // 2b. If no:
+            // Player Y starts turn (repeat from Step 1)
 })();
