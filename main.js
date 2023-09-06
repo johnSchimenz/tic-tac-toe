1ad05478456c60bfb8a023dfbfbcabe2d063397c
John Schimenz <john.schimenz@gmail.com>
2023-09-06 03:33:14 -0500

    Add return statemetns to checkWinner module

--- main.js
+++ main.js
@@ -23,24 +23,25 @@ const gameFlow = (() => {
     let randomOpenSpace = Math.floor(Math.random() * openSpaces.length);
     gameBoard[randomOpenSpace] = "X";
     console.log(gameBoard);
+})();
 
-    // 2. Check whether you have 3-in-a-row
+// 2. Check whether you have 3-in-a-row (logic good, but details aren't good)
+const checkWinner = (() => {
      // 2a. If yes:
             // print "Player X wins"
             // prompt whether to play again
-    if (gameBoard[0] === "X" && gameBoard[1] === "X" && gameBoard[2] === "X" ||
-    gameBoard[3] === "X" && gameBoard[4] === "X" && gameBoard[5] === "X" ||
-    gameBoard[6] === "X" && gameBoard[7] === "X" && gameBoard[8] === "X" ||
-    gameBoard[0] === "X" && gameBoard[3] === "X" && gameBoard[6] === "X" ||
-    gameBoard[1] === "X" && gameBoard[4] === "X" && gameBoard[7] === "X" ||
-    gameBoard[2] === "X" && gameBoard[5] === "X" && gameBoard[8] === "X" ||
-    gameBoard[0] === "X" && gameBoard[4] === "X" && gameBoard[8] === "X" ||
-    gameBoard[2] === "X" && gameBoard[4] === "X" && gameBoard[6] === "X") {
-        console.log("Player X wins");
-        console.log("Play again?");
-    // 2b. If no:
-        // Player Y starts turn (repeat from Step 1)
-    } else {
-        console.log("Player Y's turn");
-    }
+            if (gameBoard[0] === "X" && gameBoard[1] === "X" && gameBoard[2] === "X" ||
+            gameBoard[3] === "X" && gameBoard[4] === "X" && gameBoard[5] === "X" ||
+            gameBoard[6] === "X" && gameBoard[7] === "X" && gameBoard[8] === "X" ||
+            gameBoard[0] === "X" && gameBoard[3] === "X" && gameBoard[6] === "X" ||
+            gameBoard[1] === "X" && gameBoard[4] === "X" && gameBoard[7] === "X" ||
+            gameBoard[2] === "X" && gameBoard[5] === "X" && gameBoard[8] === "X" ||
+            gameBoard[0] === "X" && gameBoard[4] === "X" && gameBoard[8] === "X" ||
+            gameBoard[2] === "X" && gameBoard[4] === "X" && gameBoard[6] === "X") {
+                return console.log("Player X wins. Play again?");
+            // 2b. If no:
+                // Player Y starts turn (repeat from Step 1)
+            } else {
+                return console.log("Player Y's turn");
+            }
 })();
