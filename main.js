4cc6387384e5a364fbd3252a841eec692c22f231
John Schimenz <john.schimenz@gmail.com>
2023-09-05 22:08:56 -0500

    Got Player X to randomly select an empty cell and started creating all the win conditions

--- main.js
+++ main.js
@@ -13,9 +13,31 @@ const Player = (name, marker) => {
 
 // Game-flow object
 const gameFlow = (() => {
-    // 1. Player X selects an cell
-    
-    // 2. Check whether you have 3-in-a-row
+    // 1. Player X randomly selects an empty cell
+    const openSpaces = [];
+    for (let i = 0; i < gameBoard.length; i++) {
+        if (gameBoard[i] === "") {
+            openSpaces.push(i);
+        }
+    }
+    let randomOpenSpace = Math.floor(Math.random() * openSpaces.length);
+    gameBoard[randomOpenSpace] = "X";
+    console.log(gameBoard);
+
+    // 2. Check whether you have 3-in-a-row (RESTART HERE)
+    if (gameBoard[0] === "X" && gameBoard[1] === "X" && gameBoard[2] === "X" ||
+    gameBoard[3] === "X" && gameBoard[4] === "X" && gameBoard[5] === "X" ||
+    gameBoard[6] === "X" && gameBoard[7] === "X" && gameBoard[8] === "X" ||
+    gameBoard[0] === "X" && gameBoard[3] === "X" && gameBoard[6] === "X" ||
+    gameBoard[1] === "X" && gameBoard[4] === "X" && gameBoard[7] === "X" ||
+    gameBoard[2] === "X" && gameBoard[5] === "X" && gameBoard[8] === "X" ||
+    gameBoard[0] === "X" && gameBoard[4] === "X" && gameBoard[8] === "X" ||
+    gameBoard[2] === "X" && gameBoard[4] === "X" && gameBoard[6] === "X" ||
+
+
+
+
+    )
         // 2a. If yes:
             // print "Player X wins"
             // prompt whether to play again
