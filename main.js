eca95873ed6f4c7e065181858d30538f44d7b0a5
John Schimenz <john.schimenz@gmail.com>
2023-09-07 09:19:12 -0500

    Need to fix issue where openArray needs to update once a player has played his/her turn

--- main.js
+++ main.js
@@ -19,34 +19,78 @@ const gameFlow = (() => {
     const playerTwo = Player("Bob", "X");
     const arrayPlayers = [playerOne, playerTwo];
 
-    // 2. Create array to store empty cells
-    const openSpaces = [];
+    // 2. Create open array
+    let openArray = [];
 
-    // 3. playerOne randomly selects an empty cell
+//////////////////////////////////////////////////////////////////
+
+    // playerOne's 1st Turn
+    // 3. Figure out which cells are open on gameBoard
+    for (let i = 0; i < gameBoard.length; i++) {
+        if (gameBoard[i] !== playerOne.marker || gameBoard[i] !== playerTwo.marker) {
+            openArray.push(i);
+        }
+    }
+    console.log(openArray);
+
+    // 4. Randomly pick an open cell on gameBoard
+    let randomOpenSpacePosition = Math.floor(Math.random() * openArray.length); // get position of open space
+    console.log(randomOpenSpacePosition);
+    let randomOpenSpaceValue = openArray[randomOpenSpacePosition]; // get value at chosen open space position
+    console.log(randomOpenSpaceValue);
+
+    // 5. Replace gameBoard value with playerOne's marker
+    gameBoard[randomOpenSpaceValue] = playerOne.marker;
+    console.log(gameBoard);
+
+    // 6. Reset openArray
+    openArray = [];
+
+    // playerTwo's 1st Turn
+    // 3. Figure out which cells are open on gameBoard
+    for (let i = 0; i < gameBoard.length; i++) {
+        if (gameBoard[i] !== playerOne.marker || gameBoard[i] !== playerTwo.marker) {
+            openArray.push(i);
+        }
+    }
+    console.log(openArray);
+
+    // 4. Randomly pick an open cell on gameBoard
+    randomOpenSpacePosition = Math.floor(Math.random() * openArray.length); // get position of open space
+    console.log(randomOpenSpacePosition);
+    randomOpenSpaceValue = openArray[randomOpenSpacePosition]; // get value at chosen open space position
+    console.log(randomOpenSpaceValue);
+
+    // 5. Replace gameBoard value with playerTwo's marker
+    gameBoard[randomOpenSpaceValue] = playerTwo.marker;
+    console.log(gameBoard);
+
+   // 6. Reset openArray
+   openArray = [];
+
+//////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+    /* 2. Create array to store empty cells
+    const openSpaces = [];
     for (let i = 0; i < gameBoard.length; i++) {
         if (gameBoard[i] === "") {
             openSpaces.push(i);
         }
     }
-    let randomOpenSpace = Math.floor(Math.random() * openSpaces.length);
-    gameBoard[randomOpenSpace] = playerOne.marker;
-    console.log(gameBoard);
 
-    // 4. playerTwo randomly selects an empty cell
-    randomOpenSpace = Math.floor(Math.random() * openSpaces.length);
-    gameBoard[randomOpenSpace] = playerTwo.marker;
+    // 3. playerOne randomly selects an empty cell
+    gameBoard[randomOpenSpace] = playerOne.marker; // put marker on digit location
+    console.log(randomOpenSpace);
+    openSpaces.splice(randomOpenSpace, 1, "Closed"); // 
+    console.log(openSpaces);
     console.log(gameBoard);
+    */
 
-    // 5. Alternate turns back and forth
-
-    randomOpenSpace = Math.floor(Math.random() * openSpaces.length);
-    gameBoard[randomOpenSpace] = playerOne.marker;
-    console.log(gameBoard);
-
-    // 4. playerTwo randomly selects an empty cell
-    randomOpenSpace = Math.floor(Math.random() * openSpaces.length);
-    gameBoard[randomOpenSpace] = playerTwo.marker;
-    console.log(gameBoard);
 
     // **6. check if current player wins
 
